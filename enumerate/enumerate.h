17c8441f2f8e7b8a63c72000c9ec12666a2080d5
Mathieu Garaud <mathieu.garaud@gmail.com>
2017-12-14 17:48:55 +0100

    Initial Commit

---
+++ enumerate/enumerate.h
@@ -0,0 +1,19 @@
+//
+//  enumerate.h
+//  enumerate
+//
+//  Created by Mathieu Garaud on 14/12/2017.
+//  Copyright © 2017 Pretty Simple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for enumerate.
+FOUNDATION_EXPORT double enumerateVersionNumber;
+
+//! Project version string for enumerate.
+FOUNDATION_EXPORT const unsigned char enumerateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <enumerate/PublicHeader.h>
+
+
